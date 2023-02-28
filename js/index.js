74a83fe0512f5bef1f25eb72f992c9055573be2b
feliperjj <felipeoliveirabento@gmail.com>
2023-02-28 11:12:52 -0300

    Alteração

---
+++ js/index.js
@@ -0,0 +1,5 @@
+import initSlides from './modules/slides.js';
+import initModal from "./modules/modal.js";
+initSlides();
+initModal();
+
---
+++ js/modules/slides.js
@@ -0,0 +1,29 @@
+export default function initSlides(){
+  
+const slideshows = document.querySelectorAll('[data-component="slideshow"]');
+slideshows.forEach(initSlideShow);
+
+function initSlideShow(slideshow) {
+
+	const slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
+
+	let index = 0, time = 5000;
+	slides[index].classList.add('active');
+
+	setInterval( () => {
+		slides[index].classList.remove('active');
+		
+		index++;
+		if (index === slides.length) index = 0;
+
+		slides[index].classList.add('active');
+
+	}, time);
+}
+}
+
+
+
+  
+  
+  
