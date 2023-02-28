c4aeff9e64c2aa1d0244a3e65a6cead28775fee4
feliperjj <felipeoliveirabento@gmail.com>
2023-02-28 12:55:15 -0300

    Atualizações

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 import initSlides from './modules/slides.js';
 import initModal from "./modules/modal.js";
+import initScrollSuave from './modules/scrollSuave.js';
+initScrollSuave();
 initSlides();
 initModal();
 
